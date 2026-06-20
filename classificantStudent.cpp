f59229b868a6bf7b9d8de8045cb2f6eda85392f7
Unforgiven-DEV <23520180@gmi.uit.edu.vn>
2026-06-20 09:57:11 +0700

    Them file .gitignore va xoa cac file exe

---
+++ classificantStudent.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Toi ưu hoa doc/ghi du lieu
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Khai bao dinh dang bien
+    int n;
+
+    // Vong lap xet diem so thuc
+    for (int i = 0; i < n; i++){
+
+        //Khai bao dinh dang bien diem so cua hoc sinh
+        double a;
+        // Doc du lieu tu dau vao
+        if (cin >> a){
+            if (n >= 10 )
+        }
+    }
+
+}
